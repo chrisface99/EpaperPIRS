4055f09cd54a935789b0837864a58b4f984640cc
Chris_Pulawski <76159619+chrisface99@users.noreply.github.com>
2025-05-23 13:10:10 +0200

    Add files via upload

--- widget/widget.js
+++ widget/widget.js
@@ -1,5 +1,5 @@
 
-function select(id){ 
+function select(id) {
     // update Property Panel on country button click
     channel.publish("data", {
         type: "widgetManifest",
@@ -9,7 +9,7 @@ function select(id){
                 {
                     id: "mapClick",
                     value: ""
-                },  
+                },
                 // display Property Panel's elements with lat and long + update their values 
                 {
                     id: "long",
@@ -25,9 +25,9 @@ function select(id){
         }
     }
     )
-    
+
     // hide buttons after selectiom
-    for (let i=0; i<3; i++) {
+    for (let i = 0; i < 3; i++) {
         buttons[i].style.display = "none";
     }
     // return to timetable view
@@ -39,7 +39,7 @@ function select(id){
     selection = id
 }
 
-function handleConfig(config){
+function handleConfig(config) {
     // for debugging
     console.log("config: ", JSON.stringify(config))
 
@@ -51,22 +51,22 @@ function handleConfig(config){
             // display map on the Widget when the map button (in Property Panel) is clicked
             // display appropriate header if User wants to change location
             // display buttons on the Widget to enable location selection
-         body.style.backgroundImage = "url(./map.jpg)";
-         header.innerText = "Select location..."
-         for (let i=0; i<3; i++) {
-            buttons[i].style.display = "block";
+            body.style.backgroundImage = "url(./map.jpg)";
+            header.innerText = "Select location..."
+            for (let i = 0; i < 3; i++) {
+                buttons[i].style.display = "block";
+            }
         }
-        }
-       
+
         // if the map button has been clicked twice, cancel operation
-        else if (counter > 1){
+        else if (counter > 1) {
             console.log("reset counter, revert")
-            counter = 0 
-            for (let i=0; i<3; i++) {
+            counter = 0
+            for (let i = 0; i < 3; i++) {
                 buttons[i].style.display = "none";
             }
             console.log(selection)
-            switch (selection){
+            switch (selection) {
                 case "Spain":
                     select("Spain")
                     break;
@@ -81,15 +81,15 @@ function handleConfig(config){
                     header.innerText = "Default location"
                     break;
                 default:
-                    console.log("something went wrong when reverting to previous state")                    
+                    console.log("something went wrong when reverting to previous state")
             }
         }
-    
-  
+
+
     }
 }
 
-        function makeEditable(element) {
+     function makeEditable(element) {
             if (element.isContentEditable) return;
             
             element.contentEditable = true;
@@ -128,9 +128,9 @@ function handleConfig(config){
                 document.querySelectorAll('.item-name').forEach(function(el) {
                     const length = el.innerText.length;
                     if (length > 12) {
-                        el.style.fontSize = Math.max(1.0, 1.3 - (length - 12) * 0.05) + 'rem';
+                        el.style.fontSize = Math.max(0.9, 1.1 - (length - 12) * 0.03) + 'rem';
                     } else {
-                        el.style.fontSize = '1.3rem';
+                        el.style.fontSize = '1.1rem';
                     }
                 });
             }
